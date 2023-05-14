23dc1b217930b11c7a22dea638411df387866a87
pryority <matthewapryor@gmail.com>
2023-05-14 04:31:38 -0400

    add: svgs for dashboard item images

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,20 +1,60 @@
 import DashboardCard from "@/components/DashboardCard";
 import React from "react";
+import mooringPlan from "../../assets/images/mooring-plan.jpeg";
+import directorsList from "../../assets/images/directors-list.svg";
+import workHours from "../../assets/images/work-hours.svg";
+import clubhouseBooking from "../../assets/images/clubhouse-booking.svg";
+import howToInfo from "../../assets/images/how-to-info.svg";
+import meetingNotes from "../../assets/images/meeting-notes.svg";
+import cycEmails from "../../assets/images/cyc-emails.svg";
+import photoArchive from "../../assets/images/photo-archive.svg";
+import usefulLinks from "../../assets/images/useful-links.svg";
+import committeeList from "../../assets/images/committee-list.svg";
 
 type Props = {};
 
 const page = (props: Props) => {
   const data = [
-    "Mooring Plan",
-    "Directors List",
-    "Work Hours",
-    "Clubhouse Booking",
-    "How-To Info",
-    "Meeting Notes",
-    "CYC Emails",
-    "Photo Archive",
-    "Useful Links",
-    "Committee List",
+    {
+      name: "Mooring Plan",
+      image: mooringPlan,
+    },
+    {
+      name: "Directors List",
+      image: directorsList,
+    },
+    {
+      name: "Work Hours",
+      image: workHours,
+    },
+    {
+      name: "Clubhouse Booking",
+      image: clubhouseBooking,
+    },
+    {
+      name: "How To Info",
+      image: howToInfo,
+    },
+    {
+      name: "Meeting Notes",
+      image: meetingNotes,
+    },
+    {
+      name: "CYC Emails",
+      image: cycEmails,
+    },
+    {
+      name: "Photo Archive",
+      image: photoArchive,
+    },
+    {
+      name: "Useful Links",
+      image: usefulLinks,
+    },
+    {
+      name: "Committee List",
+      image: committeeList,
+    },
   ];
   return (
     <section className="flex flex-col w-full items-center">
@@ -24,11 +64,12 @@ const page = (props: Props) => {
         </div>
 
         <div className="grid w-full h-full gap-2 grid-cols-2 sm:grid-cols-3 md:grid-cols-4 xl:grid-cols-5">
-          {data.map((name) => (
+          {data.map((item) => (
             <DashboardCard
-              key={name}
-              name={name}
-              link={name.toLowerCase().replace(/\s/g, "-")}
+              key={item.name}
+              name={item.name}
+              link={item.name.toLowerCase().replace(/\s/g, "-")}
+              image={item.image}
             />
           ))}
         </div>
--- src/components/DashboardCard.tsx
+++ src/components/DashboardCard.tsx
@@ -1,4 +1,4 @@
-import Image from "next/image";
+import Image, { StaticImageData } from "next/image";
 import React from "react";
 import mooringPlan from "../assets/images/mooring-plan.jpeg";
 import Link from "next/link";
@@ -6,15 +6,16 @@ import Link from "next/link";
 type Props = {
   name: string;
   link: string;
+  image: StaticImageData;
 };
 
-const DashboardCard = ({ name, link }: Props) => {
+const DashboardCard = ({ name, link, image }: Props) => {
   return (
     <Link
       href={link}
       className="col-span-1 h-32 sm:h-40 md:h-56 xl:h-64 object-cover border rounded overflow-hidden relative cursor-pointer"
     >
-      <div className="flex flex-col h-full w-full justify-end p-2 md:p-2 bg-blue-900 z-20">
+      <div className="flex flex-col h-full w-full justify-end p-2 md:p-2 bg-blue-50 z-20">
         <div className="flex flex-col w-fit p-1 md:p-2 bg-blue-900/[96%] border-2 border-sky-700/95 rounded-lg px-3 md:px-4 z-20 cursor-default">
           <h4 className="z-10 text-xs md:text-md leading-3 md:leading-[20px] text-sky-50 font-bold uppercase">
             {" "}
@@ -22,7 +23,7 @@ const DashboardCard = ({ name, link }: Props) => {
           </h4>
         </div>
       </div>
-      <Image src={mooringPlan} alt="" fill className="" />
+      <Image src={image} alt="" fill />
     </Link>
   );
 };
